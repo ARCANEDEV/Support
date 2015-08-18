fbee1459ab3ea621fe213afdf33640b3ac431794
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-08-18 17:04:02 +0100

    Adding Collection class

---
+++ src/Collection.php
@@ -0,0 +1,9 @@
+<?php namespace Arcanedev\Support;
+
+use Illuminate\Support\Collection as IlluminateCollection;
+
+/**
+ * Class Collection
+ * @package Arcanedev\Support
+ */
+class Collection extends IlluminateCollection {}
