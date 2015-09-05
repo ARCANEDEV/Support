a4b5ad2a9408c4a0de19101c1e2e24199bc75030
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-05 17:20:30 +0100

    Adding RouteRegister class

---
+++ src/Laravel/RouteRegister.php
@@ -0,0 +1,107 @@
+<?php namespace Arcanedev\Support\Laravel;
+
+use Closure;
+use Illuminate\Contracts\Routing\Registrar;
+
+/**
+ * Class RouteRegister
+ * @package Arcanedev\Support\Laravel
+ */
+abstract class RouteRegister
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /** @var Registrar */
+    protected $router;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Map routes.
+     *
+     * @param  Registrar  $router
+     */
+    public function map(Registrar $router)
+    {
+        $this->setRegister($router);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getter & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * @param  Registrar  $router
+     *
+     * @return self
+     */
+    public function setRegister(Registrar $router)
+    {
+        $this->router = $router;
+
+        return $this;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Route registration Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register a new GET route with the router.
+     *
+     * @param  string                $uri
+     * @param  Closure|array|string  $action
+     */
+    public function get($uri, $action)
+    {
+        $this->router->get($uri, $action);
+    }
+
+    /**
+     * Register a new POST route with the router.
+     *
+     * @param  string                $uri
+     * @param  Closure|array|string  $action
+     */
+    public function post($uri, $action)
+    {
+        $this->router->post($uri, $action);
+    }
+
+    /**
+     * Register a new PUT route with the router.
+     *
+     * @param  string                $uri
+     * @param  Closure|array|string  $action
+     */
+    public function put($uri, $action)
+    {
+        $this->router->put($uri, $action);
+    }
+
+    /**
+     * Register a new DELETE route with the router.
+     *
+     * @param  string                $uri
+     * @param  Closure|array|string  $action
+     */
+    public function delete($uri, $action)
+    {
+        $this->router->delete($uri, $action);
+    }
+
+    /**
+     * Create a route group with shared attributes.
+     *
+     * @param  array    $attributes
+     * @param  Closure  $callback
+     */
+    protected function group(array $attributes, Closure $callback)
+    {
+        $this->router->group($attributes, $callback);
+    }
+}
