6e0cf897f99a613bdfa9fe10c76cdf23112c2af8
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-05 17:33:02 +0100

    Adding Event Service Provider

---
+++ src/Laravel/Providers/EventServiceProvider.php
@@ -0,0 +1,11 @@
+<?php namespace Arcanedev\Support\Laravel\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+/**
+ * Class EventServiceProvider
+ * @package Arcanedev\Support\Laravel\Providers
+ */
+abstract class EventServiceProvider extends ServiceProvider
+{
+}
