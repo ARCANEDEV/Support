0a147453cf049c7e07e38433ac2c39442192488b
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-14 10:44:34 +0100

    Adding some bases

---
+++ src/Bases/Policy.php
@@ -0,0 +1,19 @@
+<?php namespace Arcanedev\Support\Bases;
+
+/**
+ * Class     Policy
+ *
+ * @package  Arcanedev\Support\Bases
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+abstract class Policy
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ src/Bases/Seeder.php
@@ -0,0 +1,12 @@
+<?php namespace Arcanedev\Support\Bases;
+
+/**
+ * Class     Seeder
+ *
+ * @package  Arcanedev\Support\Bases
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+abstract class Seeder
+{
+    //
+}
