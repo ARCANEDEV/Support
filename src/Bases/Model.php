a56974e420a1a25af7be1e43558986e9fec33b02
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-26 19:17:32 +0100

    Adding Base Model

--- src/Bases/Model.php
+++ src/Bases/Model.php
@@ -10,5 +10,46 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
  */
 abstract class Model extends Eloquent
 {
-    //
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The table prefix.
+     *
+     * @var string
+     */
+    protected $prefix       = '';
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Create a new Eloquent model instance.
+     *
+     * @param  array  $attributes
+     */
+    public function __construct($attributes = [])
+    {
+        if ($this->isPrefixed()) {
+            $this->table = $this->prefix . $this->table;
+        }
+
+        parent::__construct($attributes);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Check if table is prefixed.
+     *
+     * @return bool
+     */
+    protected function isPrefixed()
+    {
+        return ! empty($this->prefix);
+    }
 }
