17e799b4e9aef102a6a3c401cd34487399d2d487
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-04 19:59:19 +0100

    Adding FormRequest basic methods

--- src/Bases/FormRequest.php
+++ src/Bases/FormRequest.php
@@ -10,5 +10,24 @@ use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
  */
 abstract class FormRequest extends BaseFormRequest
 {
-    //
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract public function rules();
 }
