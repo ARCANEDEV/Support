8662c2c08131ba21218f0b056e8f6b67c3d7381e
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-08-06 13:58:23 +0100

    Extract the Prefix methods to a trait

--- src/Bases/Model.php
+++ src/Bases/Model.php
@@ -1,5 +1,6 @@
 <?php namespace Arcanedev\Support\Bases;
 
+use Arcanedev\Support\Traits\PrefixedModel;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 /**
@@ -10,66 +11,5 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
  */
 abstract class Model extends Eloquent
 {
-    /* ------------------------------------------------------------------------------------------------
-     |  Properties
-     | ------------------------------------------------------------------------------------------------
-     */
-    /**
-     * The table prefix.
-     *
-     * @var string|null
-     */
-    protected $prefix;
-
-    /* ------------------------------------------------------------------------------------------------
-     |  Getters & Setters
-     | ------------------------------------------------------------------------------------------------
-     */
-    /**
-     * Get the table associated with the model.
-     *
-     * @return string
-     */
-    public function getTable()
-    {
-        return $this->getPrefix() . parent::getTable();
-    }
-
-    /**
-     * Get the prefix table associated with the model.
-     *
-     * @return null|string
-     */
-    public function getPrefix()
-    {
-        return $this->isPrefixed() ? $this->prefix : '';
-    }
-
-    /**
-     * Set the prefix table associated with the model.
-     *
-     * @param  string  $prefix
-     *
-     * @return self
-     */
-    public function setPrefix($prefix)
-    {
-        $this->prefix = $prefix;
-
-        return $this;
-    }
-
-    /* ------------------------------------------------------------------------------------------------
-     |  Check Functions
-     | ------------------------------------------------------------------------------------------------
-     */
-    /**
-     * Check if table is prefixed.
-     *
-     * @return bool
-     */
-    public function isPrefixed()
-    {
-        return ! is_null($this->prefix);
-    }
+    use PrefixedModel;
 }
