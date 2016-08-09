18e12e27e905bc37216f5d3eb807adadf23a282c
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-08-09 19:17:27 +0100

    Removing the whereIn method from the ARCANEDEV's Collection

--- src/Collection.php
+++ src/Collection.php
@@ -11,33 +11,13 @@ use Illuminate\Support\Collection as IlluminateCollection;
 class Collection extends IlluminateCollection
 {
     /* ------------------------------------------------------------------------------------------------
-     |  Illuminate Functions
-     | ------------------------------------------------------------------------------------------------
-     */
-    /**
-     * Filter items by the given key value pair.
-     *
-     * @param  string  $key
-     * @param  array   $values
-     * @param  bool    $strict
-     *
-     * @return static
-     */
-    public function whereIn($key, array $values, $strict = true)
-    {
-        return $this->filter(function ($item) use ($key, $values, $strict) {
-            return in_array(data_get($item, $key), $values, $strict);
-        });
-    }
-
-    /* ------------------------------------------------------------------------------------------------
      |  Custom Functions
      | ------------------------------------------------------------------------------------------------
      */
     /**
      * Reset the collection.
      *
-     * @return self
+     * @return static
      */
     public function reset()
     {
