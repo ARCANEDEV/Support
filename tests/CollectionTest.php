fe683c44634ef122e4f04520fcabfb971552a90e
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-09-04 23:21:36 +0100

    Adding unique and uniqueStrict to Collection

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -15,6 +15,70 @@ class CollectionTest extends TestCase
      | ------------------------------------------------------------------------------------------------
      */
     /** @test */
+    public function it_can_filter_unique()
+    {
+        $c = new Collection(['Hello', 'World', 'World']);
+
+        $this->assertEquals(['Hello', 'World'], $c->unique()->all());
+
+        $c = new Collection([[1, 2], [1, 2], [2, 3], [3, 4], [2, 3]]);
+
+        $this->assertEquals([[1, 2], [2, 3], [3, 4]], $c->unique()->values()->all());
+    }
+
+    /** @test */
+    public function it_can_filter_unique_with_callback()
+    {
+        $c = new Collection([
+            1 => ['id' => 1, 'first' => 'Taylor', 'last' => 'Otwell'], 2 => ['id' => 2, 'first' => 'Taylor', 'last' => 'Otwell'],
+            3 => ['id' => 3, 'first' => 'Abigail', 'last' => 'Otwell'], 4 => ['id' => 4, 'first' => 'Abigail', 'last' => 'Otwell'],
+            5 => ['id' => 5, 'first' => 'Taylor', 'last' => 'Swift'], 6 => ['id' => 6, 'first' => 'Taylor', 'last' => 'Swift'],
+        ]);
+
+        $this->assertEquals([
+            1 => ['id' => 1, 'first' => 'Taylor', 'last' => 'Otwell'],
+            3 => ['id' => 3, 'first' => 'Abigail', 'last' => 'Otwell'],
+        ], $c->unique('first')->all());
+
+        $this->assertEquals([
+            1 => ['id' => 1, 'first' => 'Taylor', 'last' => 'Otwell'],
+            3 => ['id' => 3, 'first' => 'Abigail', 'last' => 'Otwell'],
+            5 => ['id' => 5, 'first' => 'Taylor', 'last' => 'Swift'],
+        ], $c->unique(function ($item) {
+            return $item['first'].$item['last'];
+        })->all());
+    }
+
+    /** @test */
+    public function it_can_filter_unique_with_strict_mode()
+    {
+        $c = new Collection([
+            [
+                'id' => '0',
+                'name' => 'zero',
+            ],
+            [
+                'id' => '00',
+                'name' => 'double zero',
+            ],
+            [
+                'id' => '0',
+                'name' => 'again zero',
+            ],
+        ]);
+        $this->assertEquals([
+            [
+                'id' => '0',
+                'name' => 'zero',
+            ],
+            [
+                'id' => '00',
+                'name' => 'double zero',
+            ],
+        ], $c->uniqueStrict('id')->all());
+    }
+
+    /** @test */
     public function it_can_reset()
     {
         $items  = [
