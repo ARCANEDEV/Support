6548c4657725145616bba6db33c2eb0d39cbdfc3
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-12-18 19:22:24 +0000

    Cleaning CommandServiceProvider

--- src/Providers/CommandServiceProvider.php
+++ src/Providers/CommandServiceProvider.php
@@ -1,6 +1,5 @@
 <?php namespace Arcanedev\Support\Providers;
 
-use Arcanedev\Support\Exceptions\PackageException;
 use Arcanedev\Support\ServiceProvider;
 
 /**
@@ -16,20 +15,6 @@ abstract class CommandServiceProvider extends ServiceProvider
      | ------------------------------------------------------------------------------------------------
      */
     /**
-     * Vendor name.
-     *
-     * @var string
-     */
-    protected $vendor   = 'arcanedev';
-
-    /**
-     * Package name.
-     *
-     * @var string
-     */
-    protected $package  = '';
-
-    /**
      * The commands to be registered.
      *
      * @var array
@@ -42,36 +27,6 @@ abstract class CommandServiceProvider extends ServiceProvider
      * @var bool
      */
     protected $defer = true;
-
-    /* ------------------------------------------------------------------------------------------------
-     |  Getters & Setters
-     | ------------------------------------------------------------------------------------------------
-     */
-    /**
-     * Get command prefix name.
-     *
-     * @return string
-     */
-    protected function getCommandPrefix()
-    {
-        $this->checkPackageName();
-
-        return empty($this->vendor)
-            ? $this->package
-            : "{$this->vendor}.{$this->package}";
-    }
-
-    /**
-     * Get abstract command name.
-     *
-     * @param  string  $name
-     *
-     * @return string
-     */
-    protected function getAbstractCommandName($name)
-    {
-        return  $this->getCommandPrefix() . ".commands.$name";
-    }
 
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
@@ -93,36 +48,5 @@ abstract class CommandServiceProvider extends ServiceProvider
     public function provides()
     {
         return $this->commands;
-    }
-
-    /**
-     * Register a command.
-     *
-     * @param  string                $name
-     * @param  \Closure|string|null  $callback
-     */
-    protected function registerCommand($name, $callback)
-    {
-        $command = $this->getAbstractCommandName($name);
-
-        $this->singleton($command, $callback);
-
-        $this->commands[] = $command;
-    }
-
-    /* ------------------------------------------------------------------------------------------------
-     |  Check Functions
-     | ------------------------------------------------------------------------------------------------
-     */
-    /**
-     * Check the package name.
-     *
-     * @throws PackageException
-     */
-    private function checkPackageName()
-    {
-        if (empty($this->package)) {
-            throw new PackageException('You must specify the package name.');
-        }
     }
 }
