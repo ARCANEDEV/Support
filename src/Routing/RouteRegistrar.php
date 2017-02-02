dd4dc517a4d9d72a3adbd9ddf937288299fda2b8
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-02-02 18:00:39 +0000

    Refactoring the RouteRegistrar

--- src/Routing/RouteRegistrar.php
+++ src/Routing/RouteRegistrar.php
@@ -1,76 +1,49 @@
 <?php namespace Arcanedev\Support\Routing;
 
-use Closure;
-use Illuminate\Routing\Router;
-use Illuminate\Routing\RouteRegistrar as IlluminateRouteRegistrar;
-
 /**
  * Class     RouteRegistrar
  *
  * @package  Arcanedev\Support\Laravel
  * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
  *
- * @method  \Arcanedev\Support\Routing\RouteRegistrar  prefix(string $prefix)
- * @method  \Arcanedev\Support\Routing\RouteRegistrar  name(string $name)
  * @method  \Arcanedev\Support\Routing\RouteRegistrar  as(string $name)
- * @method  \Arcanedev\Support\Routing\RouteRegistrar  namespace(string $namespace)
  * @method  \Arcanedev\Support\Routing\RouteRegistrar  domain(string $domain)
+ * @method  \Arcanedev\Support\Routing\RouteRegistrar  middleware(string $middleware)
+ * @method  \Arcanedev\Support\Routing\RouteRegistrar  name(string $name)
+ * @method  \Arcanedev\Support\Routing\RouteRegistrar  namespace(string $namespace)
+ * @method  \Arcanedev\Support\Routing\RouteRegistrar  prefix(string $prefix)
+ * @method  void                                       group(...$mixed)
+ *
+ * @method  \Illuminate\Routing\Route  get($uri, $action = null)
+ * @method  \Illuminate\Routing\Route  post($uri, $action = null)
+ * @method  \Illuminate\Routing\Route  put($uri, $action = null)
+ * @method  \Illuminate\Routing\Route  patch($uri, $action = null)
+ * @method  \Illuminate\Routing\Route  delete($uri, $action = null)
+ * @method  \Illuminate\Routing\Route  options($uri, $action = null)
+ * @method  \Illuminate\Routing\Route  any($uri, $action = null)
+ * @method  \Illuminate\Routing\Route  match($methods, $uri, $action = null)
+ *
+ * @method  void                       resource($name, $controller, array $options = [])
+ * @method  void                       resources(array $resources)
+ *
+ * @method  void                       pattern($key, $pattern)
+ * @method  void                       patterns($patterns)
+ *
+ * @method  void                       model($key, $class, \Closure $callback = null)
+ * @method  void                       bind($key, $binder)
  */
-abstract class RouteRegistrar extends IlluminateRouteRegistrar
+abstract class RouteRegistrar
 {
-    /* ------------------------------------------------------------------------------------------------
-     |  Properties
-     | ------------------------------------------------------------------------------------------------
-     */
-    /**
-     * The router instance.
-     *
-     * @var \Illuminate\Routing\Router
-     */
-    protected $router;
-
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
      | ------------------------------------------------------------------------------------------------
      */
     /**
      * Register and map routes.
-     *
-     * @param  \Illuminate\Routing\Router  $router
      */
-    public static function register(Router $router)
+    public static function register()
     {
-        (new static($router))->map();
-    }
-
-    /**
-     * Map routes.
-     */
-    abstract public function map();
-
-    /* ------------------------------------------------------------------------------------------------
-     |  Getter & Setters
-     | ------------------------------------------------------------------------------------------------
-     */
-    /**
-     * Set a global where pattern on all routes.
-     *
-     * @param  string  $key
-     * @param  string  $pattern
-     */
-    public function pattern($key, $pattern)
-    {
-        $this->router->pattern($key, $pattern);
-    }
-
-    /**
-     * Set a group of global where patterns on all routes.
-     *
-     * @param  array  $patterns
-     */
-    public function patterns($patterns)
-    {
-        $this->router->patterns($patterns);
+        (new static)->map();
     }
 
     /* ------------------------------------------------------------------------------------------------
@@ -78,129 +51,22 @@ abstract class RouteRegistrar extends IlluminateRouteRegistrar
      | ------------------------------------------------------------------------------------------------
      */
     /**
-     * Register a new GET route with the router.
-     *
-     * @param  string                      $uri
-     * @param  \Closure|array|string|null  $action
-     *
-     * @return \Illuminate\Routing\Route
+     * Map routes.
      */
-    protected function get($uri, $action = null)
-    {
-        return $this->router->get($uri, $action);
-    }
+    abstract public function map();
 
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
+     */
     /**
-     * Register a new POST route with the router.
+     * @param  string  $name
+     * @param  array  $arguments
      *
-     * @param  string                      $uri
-     * @param  \Closure|array|string|null  $action
-     *
-     * @return \Illuminate\Routing\Route
+     * @return mixed
      */
-    protected function post($uri, $action = null)
+    public function __call($name, $arguments)
     {
-        return $this->router->post($uri, $action);
-    }
-
-    /**
-     * Register a new PUT route with the router.
-     *
-     * @param  string  $uri
-     * @param  \Closure|array|string|null  $action
-     * @return \Illuminate\Routing\Route
-     */
-    protected function put($uri, $action = null)
-    {
-        return $this->router->put($uri, $action);
-    }
-
-    /**
-     * Register a new PATCH route with the router.
-     *
-     * @param  string                      $uri
-     * @param  \Closure|array|string|null  $action
-     *
-     * @return \Illuminate\Routing\Route
-     */
-    protected function patch($uri, $action = null)
-    {
-        return $this->router->patch($uri, $action);
-    }
-
-    /**
-     * Register a new DELETE route with the router.
-     *
-     * @param  string                      $uri
-     * @param  \Closure|array|string|null  $action
-     *
-     * @return \Illuminate\Routing\Route
-     */
-    protected function delete($uri, $action = null)
-    {
-        return $this->router->delete($uri, $action);
-    }
-
-    /**
-     * Register a new OPTIONS route with the router.
-     *
-     * @param  string                      $uri
-     * @param  \Closure|array|string|null  $action
-     *
-     * @return \Illuminate\Routing\Route
-     */
-    protected function options($uri, $action = null)
-    {
-        return $this->router->options($uri, $action);
-    }
-
-    /**
-     * Register a new route responding to all verbs.
-     *
-     * @param  string                      $uri
-     * @param  \Closure|array|string|null  $action
-     *
-     * @return \Illuminate\Routing\Route
-     */
-    protected function any($uri, $action = null)
-    {
-        return $this->router->any($uri, $action);
-    }
-
-    /**
-     * Add a new route parameter binder.
-     *
-     * @param  string           $key
-     * @param  string|callable  $binder
-     */
-    protected function bind($key, $binder)
-    {
-        $this->router->bind($key, $binder);
-    }
-
-    /**
-     * Register a model binder for a wildcard.
-     *
-     * @param  string         $key
-     * @param  string         $class
-     * @param  \Closure|null  $callback
-     *
-     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
-     */
-    public function model($key, $class, Closure $callback = null)
-    {
-        $this->router->model($key, $class, $callback);
-    }
-
-    /**
-     * Clear the attributes.
-     *
-     * @return self
-     */
-    public function clear()
-    {
-        $this->attributes = [];
-
-        return $this;
+        return app('router')->$name(...$arguments);
     }
 }
